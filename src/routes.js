38ee63f90c735b6c0fc51974ed832039493ae196
andika febriansyah <82563655+afebrii@users.noreply.github.com>
2026-06-17 10:48:34 +0700

    feat: add departments module and register routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRoutes = require('./modules/auths/auth.routes')
 const jobsRoutes = require('./modules/jobs/job.routes')
+const departmentRoutes = require('./modules/departments/department.routes')
 // tambah route lain di sini, contoh:
 //const employeesRoutes   = require('./employeesRoutes');
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/jobs', jobsRoutes);
+router.use('/departments', departmentRoutes);
 
 module.exports = router;
